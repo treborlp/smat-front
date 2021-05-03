448176b87d73f1d9114cb9a39ffa436e1e34c9f6
Robert Lugo <treborlp@gmail.com>
2021-05-03 16:48:02 -0500

    Se organizo el menu del sistema

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,6 +3,96 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
+        id      : 'dashboards',
+        title   : 'Dashboards',
+        subtitle: 'Noticias recientes del sistema',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id   : 'dashboards.project',
+                title: 'Noticias',
+                type : 'basic',
+                icon : 'heroicons_outline:clipboard-check',
+                link : '/dashboards/project'
+            }
+        ]
+    },
+    {
+        id      : 'apps',
+        title   : 'Variables Ambientales',
+        subtitle: 'Recopilación de datos climáticos',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id      : 'apps.calendar',
+                title   : 'Meteorología',
+                subtitle: 'Mapa de estaciones meteorológicas',
+                type    : 'basic',
+                icon    : 'heroicons_outline:calendar',
+                link    : '/apps/calendar'
+            }
+        ]
+    },
+
+    {
+        id      : 'apps',
+        title   : 'Imagenes',
+        subtitle: 'Monitoreo visual',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id      : 'apps.calendar',
+                title   : 'Tiempo Real',
+                type    : 'basic',
+                icon    : 'heroicons_outline:calendar',
+                link    : '/apps/calendar'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'Histórico',
+                type : 'basic',
+                icon : 'heroicons_outline:user-group',
+                link : '/apps/contacts'
+            }
+            
+        ]
+    },
+    {
+        id      : 'apps',
+        title   : 'Geomática',
+        subtitle: 'Información vectorial y raster',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id      : 'apps.calendar',
+                title   : 'Geoportal',
+                type    : 'basic',
+                icon    : 'heroicons_outline:calendar',
+                link    : '/apps/calendar'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'Datos vectoriales',
+                type : 'basic',
+                icon : 'heroicons_outline:user-group',
+                link : '/apps/contacts'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'Datos Raster',
+                type : 'basic',
+                icon : 'heroicons_outline:user-group',
+                link : '/apps/contacts'
+            }
+            
+        ]
+    },
+    
+    {
         id      : 'gs',
         title   : 'Gestión Social',
         subtitle: 'Mapeo de actores internos y externos',
@@ -11,14 +101,14 @@ export const defaultNavigation: FuseNavigationItem[] = [
         children: [
             {
                 id   : 'gs.social',
-                title: 'Social',
+                title: 'Actores',
                 type : 'basic',
                 icon : 'heroicons_outline:user-group',
                 link : '/social'
             },
             {
                 id   : 'dashboards.analytics',
-                title: 'Analytics',
+                title: 'Mapa de Actores',
                 type : 'basic',
                 icon : 'heroicons_outline:chart-pie',
                 link : '/dashboards/analytics'
@@ -26,8 +116,45 @@ export const defaultNavigation: FuseNavigationItem[] = [
         ]
     },
     {
+        id      : 'apps',
+        title   : 'Inventarios',
+        subtitle: 'Estadisticas de los inventarios',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id      : 'apps.calendar',
+                title   : 'Glaciares',
+                type    : 'basic',
+                icon    : 'heroicons_outline:calendar',
+                link    : '/apps/calendar'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'Bofedales',
+                type : 'basic',
+                icon : 'heroicons_outline:user-group',
+                link : '/apps/contacts'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'Lagunas glaciares',
+                type : 'basic',
+                icon : 'heroicons_outline:user-group',
+                link : '/apps/contacts'
+            } 
+        ]
+    },
+    {
         id   : 'example',
-        title: 'Example',
+        title: 'Repositorio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'example2',
+        title: 'Inaigem Watch',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
